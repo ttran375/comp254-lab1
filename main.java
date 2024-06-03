4fe3d886e66eaa0a0f7d1464fea464af6789333d
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-03 17:45:52 +0000

    Mon, Jun 3, 2024, 5:45 PM +00:00

---
+++ main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 1000;
+        System.out.println("Hey - your input is: " + n);
+        System.out.println("Hmm.. I'm doing more stuff with: " + n);
+        System.out.println("And more: " + n);
+
+        for (int i = 1; i < n; i = i * 2) {
+            System.out.println("Hey - I'm busy looking at: " + i);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < n; j = j * 2) {
+                System.out.println("Hey - I'm busy looking at: " + i + " and " + j);
+            }
+        }
+    }
+
+    public static double[] prefixAverage1(double[] x) {
+        int n = x.length;
+        double[] a = new double[n];
+        for (int j = 0; j < n; j++) {
+            double total = 0;
+            for (int i = 0; i <= j; i++) {
+                total += x[i];
+            }
+            a[j] = total / (j + 1);
+        }
+        return a;
+    }
+
+    public static double[] prefixAverage2(double[] x) {
+        int n = x.length;
+        double[] a = new double[n];
+        double total = 0;
+        for (int j = 0; j < n; j++) {
+            total += x[j];
+            a[j] = total / (j + 1);
+        }
+        return a;
+    }
+}
